7a5d54705cb595c148f81ecbeb86de4d70a840c5
Dragoz Morgado <dragozcm@gmail.com>
2020-08-28 20:34:40 -0500

    agregando los archivos principales

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyP = require("body-parser");
+
+const app = express();
+var items = [];
+app.set("view engine", "ejs");
+app.use(bodyP.urlencoded({ extended: false }));
+
+app.listen(3000, function () {
+  console.log("I'm alive at port 3000");
+});
+
+app.get("/", function (req, res) {
+  var date = new Date();
+  //var today = date.getDay();
+  var options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+
+  var day = date.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, addItem: items });
+});
+
+app.post("/", function (req, res) {
+  //console.log(req.body.entrada);
+  var item = req.body.entrada;
+  items.push(item);
+
+  res.redirect("/");
+
+  //res.send("PAso");
+});

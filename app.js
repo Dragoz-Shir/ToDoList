1344b3a89cda9e79ef5c46977cb51620f0f51b57
Dragoz Morgado <dragozcm@gmail.com>
2020-09-04 12:58:35 -0500

    Agregado la bd y sin date.js

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 const express = require("express");
 const bodyP = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 const app = express();
-
-const items = ["Comprar comida", "Prepara la comida", "Comer :3"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyP.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todoListDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const itemSchema = {
+  name: {
+    type: String,
+    require: true,
+  },
+};
+const Item = mongoose.model("Item", itemSchema);
+
+const item = new Item({
+  name: "¡Bienvenido al TodoList!",
+});
+const item1 = new Item({
+  name: "Agrega tareas con el boton de +",
+});
+const item2 = new Item({
+  name: "<- click para borrar un item",
+}); /*
+Item.insertMany([item, item1, item2], function (e) {
+  if (e) {
+    console.log("Error al insertar");
+  } else {
+    console.log("Todo excelentet al insertar");
+  }
+});*/
 app.listen(3000, function () {
   console.log("I'm alive at port 3000");
 });
 
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { pageTitle: day, addItem: items });
+  let array = [];
+  Item.find(function (e, foundItems) {
+    foundItems.forEach(function (item) {
+      array.push(item.name);
+    });
+
+    res.render("list", { pageTitle: "Today", addItem: array });
+  });
 });
 
 app.post("/", function (req, res) {
